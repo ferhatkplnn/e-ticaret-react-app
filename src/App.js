922e52812d9acaa591d01b0ba60e6761f346301c
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-14 19:34:43 +0300

    style: Add padding to Routes component for content spacing
    This commit wraps the Routes component in a Box Component and adds 20px
    of padding to crate a visual separation from the edges. The padding
    enhances the overall layout and readability of the content.
    
    The following changes were make:
    - Wrapped Routes component in a Box component
    - Added 20px padding for content spacing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Products from "./pages/Products";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Products />} />
-      </Routes>
+      <Box p="20px">
+        <Routes>
+          <Route path="/" element={<Products />} />
+        </Routes>
+      </Box>
     </BrowserRouter>
   );
 }
