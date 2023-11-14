3946ecce9e8528d234ff41af8127400cfd0aa664
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-15 02:52:43 +0300

    chore: Integrate react-query and wrap App with QueryClientProvider
    This commit adds the @tanstack/react-query library to the project using
    npm. To use the library, the App component in the index.js file is
    wrapped with QueryClientProvider, providing the necessary context for
    query operations.
    
    The following changes were make:
    - Intalled @tanstack/react-query library
    - Wrapped App component with QueryClientProvider for query operations

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
+  </QueryClientProvider>
 );
