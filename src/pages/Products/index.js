a13aa7c165ab019f7c56d97e51224d15601f47b0
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-15 04:14:51 +0300

    feat: Implemented infinite scroll for Products page using react-query
    This commit refactored the Products page to utilize react-query for
    fetching and displaying product data with infinite scroll. The
    fetchProductList function from the api.js file was integrated with
    the useInfiniteQuery hook. The page now dynamically load and displays
    product cards as the user scrolls. Styling and layout adjustments were
    made to accommodate the new functionality.
    
    The following changes were make:
    - Implemented infinite scroll for Products page
    - Refactored Products page to use react-query for data fetching
    - Integrated fetchProductList function with usInfiniteQuery hook
    - Updated layout and styling to support infinite scroll

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,22 +1,71 @@
 import React from "react";
 import Card from "../../components/Card";
-import { Grid } from "@chakra-ui/react";
+import { Button, Flex, Grid } from "@chakra-ui/react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchProductList } from "../../api";
 
 function Products() {
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["products"],
+    queryFn: fetchProductList,
+    initialPageParam: 0,
+    getNextPageParam: (lastGroup, allGroup) => {
+      const morePagesExist = lastGroup?.length === 12;
+
+      if (!morePagesExist) return;
+
+      return allGroup.length + 1;
+    },
+  });
+
+  if (status === "pending") return <p>Loading ...</p>;
+
+  if (status === "error") return <p>Error: {error.message}</p>;
+
+  console.log(data);
+
   return (
     <div>
-      <Grid mr="15%" ml="15%" templateColumns="repeat(3, 1fr)" gap={4}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <Grid {...gridStyles}>
+        {data.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.map((product) => (
+              <Card product={product} key={product._id} />
+            ))}
+          </React.Fragment>
+        ))}
       </Grid>
+      <Flex mt="10" justifyContent="center">
+        <Button
+          onClick={() => fetchNextPage()}
+          isLoading={isFetchingNextPage}
+          isDisabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more ..."
+            : hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </Button>
+      </Flex>
+      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
     </div>
   );
 }
+
+const gridStyles = {
+  mr: "15%",
+  ml: "15%",
+  templateColumns: "repeat(3, 1fr)",
+  gap: 4,
+};
 
 export default Products;
