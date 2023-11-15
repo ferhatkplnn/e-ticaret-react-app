535041103cdac5af904866f47e2f35942a7d88d0
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-15 23:14:51 +0300

    feat: Enhance ProductDetail page for displaying product information
    Changes:
    - Enhanced ProductDetail page to display comprehensive product information
    - Adjusted components and layouts for improved presentation of product details

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,9 +1,61 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { fetchProduct } from "../../api";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import ImageGallery from "react-image-gallery";
 
 function ProductDetail() {
   const { product_id } = useParams();
-  return <div>ProductDetail {product_id}</div>;
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["product", { product_id }],
+    queryFn: () => fetchProduct(product_id),
+  });
+
+  if (isPending) {
+    return <LoadingSpinner />;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+  console.log(data);
+
+  const images =
+    data.photos?.map((url) => ({
+      original: url,
+      thumbnail: url,
+      lazyLoad: true,
+    })) || [];
+
+  return (
+    <Flex>
+      <Box flex={1}>
+        <ImageGallery items={images} />
+      </Box>
+
+      <Box flex="1" pl="20">
+        <Heading as="h1" fontSize="2xl">
+          {data.title}
+        </Heading>
+
+        <Text fontWeight={"bold"} fontSize="32">
+          {data.price}{" "}
+          <Text as="span" fontWeight="normal" fontSize="18px">
+            TL
+          </Text>
+        </Text>
+
+        <Text>{data.description}</Text>
+
+        <Text fontSize="small" fontStyle="italic" color="blackAlpha.600">
+          {data.createAt}
+        </Text>
+      </Box>
+    </Flex>
+  );
 }
 
 export default ProductDetail;
