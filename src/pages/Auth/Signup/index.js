e88e2e255753f47818b21d827bf50abfb6c11d7e
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-16 02:07:55 +0300

    feat: Design SignUp page for user registration
    Changes:
    - Created SingUp page with user-friendly design for registrarion

--- src/pages/Auth/Signup/index.js
+++ src/pages/Auth/Signup/index.js
@@ -1,7 +1,50 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 import React from "react";
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <Flex textAlign="center" width="full" justifyContent="center">
+      <Box>
+        <Box textAlign="center">
+          <Heading>Sign Up</Heading>
+        </Box>
+        <Box my="5" textAlign="left">
+          <form>
+            <FormControl>
+              <FormLabel>E-mail</FormLabel>
+              <Input placeholder="Email" name="email" type="email" />
+            </FormControl>
+
+            <FormControl mt="4">
+              <FormLabel>Password</FormLabel>
+              <Input placeholder="Password" name="password" type="password" />
+            </FormControl>
+
+            <FormControl mt="4">
+              <FormLabel>Password Confirm</FormLabel>
+              <Input
+                placeholder="Password Confirm"
+                name="passwordConfirm"
+                type="password"
+              />
+            </FormControl>
+
+            <Button w="full" colorScheme="messenger" mt="4" type="submit">
+              Sing up
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
 }
 
 export default Signup;
