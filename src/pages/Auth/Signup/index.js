047a1c8136a7830f63ec81f3f8c2f022a135eb8c
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-16 02:45:52 +0300

    feat: Integrate Fromik for enhanced form management in SignUp page
    Changes:
    - Integrated Formik library for improved form handling in SignUp page
    - Implemented useFormik hook for form state management, validation, and submission
    Updated form components to utilize Formik for handling input changes, blur events, and validation

--- src/pages/Auth/Signup/index.js
+++ src/pages/Auth/Signup/index.js
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Box,
   Button,
   Flex,
@@ -7,25 +8,63 @@ import {
   Heading,
   Input,
 } from "@chakra-ui/react";
+import { useFormik } from "formik";
 import React from "react";
 
 function Signup() {
+  const { values, touched, errors, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      },
+      onSubmit: (values, bag) => {
+        try {
+          console.log("submited");
+          console.log(values);
+          console.log(bag);
+        } catch (error) {
+          bag.setErrors({ general: error.response.data.message });
+        }
+      },
+    });
   return (
     <Flex textAlign="center" width="full" justifyContent="center">
       <Box>
         <Box textAlign="center">
           <Heading>Sign Up</Heading>
         </Box>
+        <Box my="5">
+          {errors.general && <Alert status="error">{errors.general}</Alert>}
+        </Box>
+
         <Box my="5" textAlign="left">
-          <form>
+          <form onSubmit={handleSubmit}>
             <FormControl>
               <FormLabel>E-mail</FormLabel>
-              <Input placeholder="Email" name="email" type="email" />
+              <Input
+                placeholder="Email"
+                name="email"
+                type="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                isInvalid={touched.email && errors.email}
+              />
             </FormControl>
 
             <FormControl mt="4">
               <FormLabel>Password</FormLabel>
-              <Input placeholder="Password" name="password" type="password" />
+              <Input
+                placeholder="Password"
+                name="password"
+                type="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                isInvalid={touched.password && errors.password}
+              />
             </FormControl>
 
             <FormControl mt="4">
@@ -34,11 +73,15 @@ function Signup() {
                 placeholder="Password Confirm"
                 name="passwordConfirm"
                 type="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.passwordConfirm}
+                isInvalid={touched.passwordConfirm && errors.passwordConfirm}
               />
             </FormControl>
 
             <Button w="full" colorScheme="messenger" mt="4" type="submit">
-              Sing up
+              Sign up
             </Button>
           </form>
         </Box>
