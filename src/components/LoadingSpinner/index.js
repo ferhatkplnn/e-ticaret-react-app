03711cc575bb9b2651e91da2a53086dfeb718a46
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-15 16:02:39 +0300

    feat: Add "Loading" text  to Spinner component
    Changes:
    - Added "Loading" text to Spinner component for improved user fedback

--- src/components/LoadingSpinner/index.js
+++ src/components/LoadingSpinner/index.js
@@ -1,9 +1,14 @@
-import { Flex, Spinner } from "@chakra-ui/react";
+import { Flex, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 
 function LoadingSpinner() {
   return (
-    <Flex justifyContent="center" alignItems="center" height="100vh">
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
       <Spinner
         thickness="4px"
         speed="0.65s"
@@ -11,6 +16,7 @@ function LoadingSpinner() {
         size="xl"
         color="red"
       />
+      <Text>Loading...</Text>
     </Flex>
   );
 }
