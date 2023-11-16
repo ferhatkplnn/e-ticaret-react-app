80da05eeca72cebb43b0e717edad4fccf563a75f
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-16 22:46:55 +0300

    feat(auth): Add user authentication with automatic login
    This commit enhances the AuthProvider in the context folder to handle
    user authentication. It introduces the useEffect hook to automatically
    fetch user data on component mount, triggering an automatic login if
    successful. The login function is updated to store tokens in local
    storage, and a loading state is added to display a loading spinner
    while fetching user data.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,39 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { fetchMe } from "../api";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const userData = await fetchMe();
+
+        setLoggedIn(true);
+        setUser(userData);
+      } catch (error) {
+        console.error("Error fetching the user:", error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
 
   const login = (data) => {
     setLoggedIn(true);
     setUser(data.user);
+
+    try {
+      localStorage.setItem("access-token", data.accessToken);
+      localStorage.setItem("refresh-token", data.refreshToken);
+    } catch (error) {
+      console.error("Error storing tokens:", error);
+    }
   };
 
   const authContextValues = {
@@ -16,6 +41,10 @@ const AuthProvider = ({ children }) => {
     user,
     login,
   };
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <AuthContext.Provider value={authContextValues}>
