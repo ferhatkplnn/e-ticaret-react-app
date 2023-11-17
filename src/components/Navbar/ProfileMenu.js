c5bb2e19b1e95d062f9d44c871c49a8db6b22f98
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-17 10:41:26 +0300

    feat(logotu): Add logout functionality to ProfileMenu
    This commit implements the logout functionality in the ProfileMenu
    component. Now, when the user clicks the logout button, the logout
    function is triggered, loggin the user out of the aplication.

--- src/components/Navbar/ProfileMenu.js
+++ src/components/Navbar/ProfileMenu.js
@@ -9,30 +9,34 @@ import {
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
-export const ProfileMenu = () => (
-  <Menu>
-    <MenuButton as={Button} colorScheme="linkedin">
-      Profile <ChevronDownIcon />
-    </MenuButton>
-    <MenuList boxShadow="base">
-      <MenuGroup title="Profile">
-        <Link to="/profile">
-          <MenuItem>My Account</MenuItem>
-        </Link>
-        <Link to="">
-          <MenuItem>My Basket</MenuItem>
-        </Link>
-        <Link to="">
-          <MenuItem>My Orders</MenuItem>
-        </Link>
-      </MenuGroup>
-      <MenuDivider />
-      <MenuGroup>
-        <MenuItem onClick={() => {}} color="red.600" fontWeight="bold">
-          Logout
-        </MenuItem>
-      </MenuGroup>
-    </MenuList>
-  </Menu>
-);
+export const ProfileMenu = () => {
+  const { logout } = useAuth();
+  return (
+    <Menu>
+      <MenuButton as={Button} colorScheme="linkedin">
+        Profile <ChevronDownIcon />
+      </MenuButton>
+      <MenuList boxShadow="base">
+        <MenuGroup title="Profile">
+          <Link to="/profile">
+            <MenuItem>My Account</MenuItem>
+          </Link>
+          <Link to="">
+            <MenuItem>My Basket</MenuItem>
+          </Link>
+          <Link to="">
+            <MenuItem>My Orders</MenuItem>
+          </Link>
+        </MenuGroup>
+        <MenuDivider />
+        <MenuGroup>
+          <MenuItem onClick={logout} color="red.600" fontWeight="bold">
+            Logout
+          </MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
+  );
+};
