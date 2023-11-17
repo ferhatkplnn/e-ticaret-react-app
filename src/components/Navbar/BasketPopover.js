652e41dda01b1fd3af0868e7e0b16e85c9ec574c
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-17 22:59:45 +0300

    feat: Add remove button to BasketPopover component
    This commit adds a remove button to each item in the BasketPopover component,
    enabling users to easily remove items from the basket. The layout of each basket
    item has been updated to include the remove button.
    
    Changes:
    - Added remove button to BasketPopover
    - Updated layout of basket items to accommodate the new button

--- src/components/Navbar/BasketPopover.js
+++ src/components/Navbar/BasketPopover.js
@@ -13,42 +13,57 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useBasket } from "../../context/BasketContext";
 
-const BasketPopover = ({ basket }) => (
-  <Popover>
-    <PopoverTrigger>
-      <Button colorScheme="pink" variant="outline" mr="5">
-        Basket ({basket.length})
-      </Button>
-    </PopoverTrigger>
-    <PopoverContent>
-      <PopoverArrow />
-      <PopoverCloseButton />
-      <PopoverHeader>
-        <Text fontWeight="bold" fontSize="md" textAlign="center">
-          Basket List
-        </Text>
-      </PopoverHeader>
-      <PopoverBody>
-        <OrderedList listStyleType="none">
-          {basket.map((item, index) => (
-            <ListItem key={item._id}>
-              <Button variant="link">
-                <Link to={`product/${item._id}`}>
-                  {index + 1} - {item.title} - {item.price}
-                </Link>
-              </Button>
-            </ListItem>
-          ))}
-        </OrderedList>
-      </PopoverBody>
-      <PopoverFooter>
-        <Button display="flex" as="u" variant="link" colorScheme="red">
-          <Link to="basket">Go Basket</Link>
+const BasketPopover = ({ basket }) => {
+  const { removeFromBasket } = useBasket();
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button colorScheme="pink" variant="outline" mr="5">
+          Basket ({basket.length})
         </Button>
-      </PopoverFooter>
-    </PopoverContent>
-  </Popover>
-);
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>
+          <Text fontWeight="bold" fontSize="md" textAlign="center">
+            Basket List
+          </Text>
+        </PopoverHeader>
+        <PopoverBody>
+          <OrderedList listStyleType="none">
+            {basket.map((item, index) => (
+              <ListItem
+                key={item._id}
+                display="flex"
+                justifyContent="space-between"
+              >
+                <Button variant="link">
+                  <Link to={`product/${item._id}`}>
+                    {index + 1} - {item.title} - {item.price}
+                  </Link>
+                </Button>
+                <Button
+                  color="red"
+                  variant="link"
+                  onClick={() => removeFromBasket(item)}
+                >
+                  X
+                </Button>
+              </ListItem>
+            ))}
+          </OrderedList>
+        </PopoverBody>
+        <PopoverFooter>
+          <Button display="flex" as="u" variant="link" colorScheme="red">
+            <Link to="basket">Go Basket</Link>
+          </Button>
+        </PopoverFooter>
+      </PopoverContent>
+    </Popover>
+  );
+};
 
 export default BasketPopover;
