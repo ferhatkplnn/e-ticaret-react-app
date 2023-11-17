e41259264bb501c28cf7bd43fbf90cd4c1b4ba58
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-17 21:36:45 +0300

    feat: Add addToBasket function to BasketContext
    This commit introduces the addToBasket function to the BasketContext,
    allwing components to add products to the shopping basket.

--- src/context/BasketContext.js
+++ src/context/BasketContext.js
@@ -5,7 +5,11 @@ const BasketContext = createContext();
 const BasketProvider = ({ children }) => {
   const [basket, setBasket] = useState([]);
 
-  const values = { basket, setBasket };
+  const addToBasket = (data) => {
+    setBasket((prev) => [...prev, data]);
+  };
+
+  const values = { basket, setBasket, addToBasket };
 
   return (
     <BasketContext.Provider value={values}>{children}</BasketContext.Provider>
