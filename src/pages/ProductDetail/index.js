fa4525fd48f5d407d7f174a8dcf26aafc4997f32
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-17 23:01:29 +0300

    feat: Add basket functionality to ProductDetail page
    This commit adds the functionality to display and manage the basket directly
    from the ProductDetail page. Users can now add the product to the basket,
    remove it if already added, and proceed to checkout. The layout of the page
    has been updated to include buttons for buying, adding to the basket, and
    removing from the basket.
    
    Changes:
    - Integrated basket context into ProductDetail page
    - Added buttons for buying, adding to the basket, and removing from the basket

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -3,11 +3,21 @@ import { useParams } from "react-router-dom";
 import { fetchProduct } from "../../api";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "../../components/LoadingSpinner";
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 import ImageGallery from "react-image-gallery";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { useBasket } from "../../context/BasketContext";
 
 function ProductDetail() {
   const { product_id } = useParams();
+  const { addToBasket, basket, removeFromBasket } = useBasket();
 
   const { isPending, isError, data, error } = useQuery({
     queryKey: ["product", { product_id }],
@@ -21,6 +31,8 @@ function ProductDetail() {
   if (isError) {
     return <span>Error: {error.message}</span>;
   }
+
+  const findBasketItem = basket.find((p) => p._id === data._id);
 
   const images =
     data.photos?.map((url) => ({
@@ -39,19 +51,41 @@ function ProductDetail() {
         <Heading as="h1" fontSize="2xl">
           {data.title}
         </Heading>
-
         <Text fontWeight={"bold"} fontSize="32">
           {data.price}{" "}
           <Text as="span" fontWeight="normal" fontSize="18px">
             TL
           </Text>
         </Text>
-
         <Text>{data.description}</Text>
-
         <Text fontSize="small" fontStyle="italic" color="blackAlpha.600">
           {data.createAt}
-        </Text>
+        </Text>{" "}
+        <ButtonGroup spacing="3" mt="5">
+          <Button variant="solid" colorScheme="blue">
+            Buy now
+          </Button>
+          {findBasketItem ? (
+            <Button
+              color="red"
+              variant="ghost"
+              colorScheme="blue"
+              onClick={() => {
+                removeFromBasket(data);
+              }}
+            >
+              <DeleteIcon color="red" boxSize="28px" />
+            </Button>
+          ) : (
+            <Button
+              variant="ghost"
+              colorScheme="blue"
+              onClick={() => addToBasket(data)}
+            >
+              Add to cart
+            </Button>
+          )}
+        </ButtonGroup>
       </Box>
     </Flex>
   );
