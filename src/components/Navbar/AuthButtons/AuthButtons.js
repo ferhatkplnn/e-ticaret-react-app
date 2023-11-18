ba1218e9794fb476bc4aefe29d67c196f84dde03
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-18 21:47:58 +0300

    refactor: Integrate AdminMenu into AuthButtons
    Embed the AdminMenu component within the AuthButtons
    component to conditionally render it based on the user's role.

--- src/components/Navbar/AuthButtons/AuthButtons.js
+++ src/components/Navbar/AuthButtons/AuthButtons.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { ProfileMenu } from "./ProfileMenu/ProfileMenu";
 import { useBasket } from "../../../context/BasketContext";
 import BasketPopover from "./BasketPopover/BasketPopover";
+import AdminMenu from "./ProfileMenu/AdminMenu/AdminMenu";
 
 export const AuthButtons = () => {
   const { loggedIn, user, loading } = useAuth();
@@ -26,6 +27,7 @@ export const AuthButtons = () => {
     <Box>
       {basket.length > 0 && <BasketPopover basket={basket} />}
       {user?.role === "user" && <ProfileMenu />}
+      {user?.role === "admin" && <AdminMenu />}
     </Box>
   );
 };
